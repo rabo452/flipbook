02005a772654809e0dccef0b67c93967b4ccaf1b
Dimka <usenkoaleksej878@gmail.com>
2022-09-14 00:59:05 +0100

    upload to github

--- api server/server/static/admin/js/admin_change_pas.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#sub').onclick = function() {
-  form = document.querySelector('.form');
-  if (form.new_password.value == form.repeat_password.value) form.submit()
-  else alert('Please make sure that password the same in both input')
-}
